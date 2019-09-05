55408774f21bc4687ddb7f7cab7ca78cd7b1bfcf
OJEYINKA OLANIYI PHILIP <Olaniyiojeyinka@gmail.com>
2019-09-05 15:23:50 +0100

    dynamically get site meta details odf affilate  controller from db

--- application/controllers/Affilate.php
+++ application/controllers/Affilate.php
@@ -23,12 +23,13 @@ public function __construct()
          $this->load->library(array('session','form_validation','user_agent'));
      $this->load->helper(array('url','form','page_helper','blog_helper'));
     //get details from db later (The web details)
-      $this->siteName = "Ad Network";
-      $this->author = "The author";
-      $this->keywords = "The keywords here";
-      $this->description= "the description";
-      //$this->user =  $this->advertiser_model->get_advertiser_by_id();
+     
+      $this->siteName = $this->advertiser_model->get_system_variable("site_name");
+      $this->author = $this->advertiser_model->get_system_variable("author");
+      $this->keywords = $this->advertiser_model->get_system_variable("keywords");
+      $this->description= $this->advertiser_model->get_system_variable("description");
       $this->noindex = '<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+
 }
 public function index()
 {
