4d142a5c64e433dffe73b9d69381c0594a674808
OJEYINKA OLANIYI PHILIP <Olaniyiojeyinka@gmail.com>
2019-09-10 06:46:27 +0100

    added site author and tagline inmput field to site_settings view

--- application/views/admin/site_settings_view.php
+++ application/views/admin/site_settings_view.php
@@ -1,4 +1,4 @@
-<div class="w3-twothird w3-center w3-padding">
+<div class="w3-twothird w3-center w3-padding w3-center">
 
 	<span class="w3-xlarge w3-text-blue-grey">
     SITE & SEO Settings
@@ -10,11 +10,22 @@
 <input type="text" placeholder="Site Name" name="site_name"
  value="<?=$site_name ?>"/>
 <br>
+<span class="w3-label w3-large">Site Tag Line:<span class="w3-small">
+	or Motto</span> </span><br>
+<input type="text" placeholder="Site Tagline" name="site_tagline"
+ value="<?=$site_tagline ?>"/>
+<br>
+<span class="w3-label w3-large">Site Author: </span><br>
+<input type="text" placeholder="Site Author" name="site_author"
+ value="<?=$site_author ?>"/>
+<br>
+<span
 <span class="w3-label w3-large">Site keywords:<span class="w3-small">
 	Each separated comma ','</span> </span><br>
 <input type="text" placeholder="Site Keywords" name="site_keywords"
  value="<?=$site_keywords ?>"/>
 <br>
+<span
 <span class="w3-label w3-large">Site Descriptions: </span><br>
 <textarea placeholder="Descriptions here" cols="10" rows="10">
 	<?= $site_descriptions?></textarea>
