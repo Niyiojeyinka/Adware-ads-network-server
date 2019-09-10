1021e0173020049545455eb22934d2cd65ff72aa
OJEYINKA OLANIYI PHILIP <Olaniyiojeyinka@gmail.com>
2019-09-10 06:19:42 +0100

    wrote the view interface of site_settings

--- application/views/admin/site_settings_view.php
+++ application/views/admin/site_settings_view.php
@@ -3,7 +3,25 @@
 	<span class="w3-xlarge w3-text-blue-grey">
     SITE & SEO Settings
 	</span>
+<div class="w3-padding">
+<?= form_open("admin/site_settings") ?>
 
+<span class="w3-label w3-large">Site Name: </span><br>
+<input type="text" placeholder="Site Name" name="site_name"
+ value="<?=$site_name ?>"/>
+<br>
+<span class="w3-label w3-large">Site keywords:<span class="w3-small">
+	Each separated comma ','</span> </span><br>
+<input type="text" placeholder="Site Keywords" name="site_keywords"
+ value="<?=$site_keywords ?>"/>
+<br>
+<span class="w3-label w3-large">Site Descriptions: </span><br>
+<textarea placeholder="Descriptions here" cols="10" rows="10">
+	<?= $site_descriptions?></textarea>
+
+<input type="submit" class="w3-button w3-blue w3-margin" value="Change Site Details"/>
+
+</div>
 
 
 
