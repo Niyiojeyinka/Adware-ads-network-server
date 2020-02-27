b6959e9772f886f271083c60983fd36a08431882
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-02-27 12:57:55 +0100

    removed  currency list in payment views

--- application/views/user/advertiser/payment_view.php
+++ application/views/user/advertiser/payment_view.php
@@ -9,18 +9,7 @@ if(isset($_SESSION['action_status_report']))
 
 ?>
 <?= form_open('advertiser_dashboard/payment') ?>
-<span class="w3-medium">Please choose currency you want to be bill in</span><br>
-<select name="currency" class="w3-padding">
-	<option value="usd">US Dollar</option>
-		<option value="zar">South African Rand</option>
-	<option value="NGN">Nigerian Naira</option>
-	<option value="ugx">Ughanda Shilling</option>
-		<option value="kes">Kenya Shilling</option>
-		<option value="ghs">Ghana Cedi</option>
-		<option value="tzs">Tanzanian Shilling</option>
-
-
-</select><br><br>
+<br>
 <span>Amount in Chosen Currency</span><br>
 <input type="number" name="amount" min="<?=$general_details['minimum_deposit'] ?>" class="w3-padding"/>
 <br>
