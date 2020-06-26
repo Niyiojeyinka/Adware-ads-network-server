fce28f53eb1a9a2ab04f422d94f7a25794d3f7dd
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-06-26 17:58:55 +0100

    Cleaned up CI for Laravel

--- application/views/public/form_submitted_already_view.php
+++
@@ -1,6 +0,0 @@
-<div style='margin: 24px;margin-top: 72px;' class="w3-container w3-card-8 w3-center w3-padding-jumbo">
-	<center><div style="max-width: 80%;" class="w3-center w3-margin">
-		<i class="fa fa-check w3-text-indigo w3-jumbo"></i><br>
-<span>Form Already Submitted </span><br>
-<span class="w3-tiny">The Advertiser will contact You Soon</span>
-</div></center></div>
