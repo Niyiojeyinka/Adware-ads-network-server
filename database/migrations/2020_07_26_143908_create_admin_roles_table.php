abbd00cb332b00847c0cb83a224432ec71306d4e
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-07-26 15:40:02 +0100

    added admin roles model and migrations

---
+++ database/migrations/2020_07_26_143908_create_admin_roles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_roles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_roles');
+    }
+}
