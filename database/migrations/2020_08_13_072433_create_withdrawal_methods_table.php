ff1a9a34d84c8178ea69b665995b2e33c4edd2de
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-08-13 08:25:16 +0100

    added withdrawal method and migration file

---
+++ database/migrations/2020_08_13_072433_create_withdrawal_methods_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWithdrawalMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('withdrawal_methods', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('withdrawal_methods');
+    }
+}
