2382fea71af2fd03d759b5705566dd2686db86f2
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-08-13 08:48:30 +0100

    changed blog to post

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Blog extends Model
+class Post extends Model
 {
     //
 }
--- database/migrations/2020_08_13_073840_create_posts_table.php
+++ database/migrations/2020_08_13_073840_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogsTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,13 @@ class CreateBlogsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('contents');
+            $table->text('description')->nullable();
+            $table->string('keywords')->nullable();
+            $table->enum('status', ['published', 'draft']);
             $table->timestamps();
         });
     }
@@ -26,6 +31,6 @@ class CreateBlogsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('posts');
     }
 }
