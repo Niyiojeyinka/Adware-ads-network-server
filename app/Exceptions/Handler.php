cc309b967b59b3b9bc98dfc859e54f301e793e65
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-08-14 10:19:25 +0100

    added custom exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,14 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Database\QueryException;
 
 class Handler extends ExceptionHandler
 {
@@ -21,10 +29,7 @@ class Handler extends ExceptionHandler
      *
      * @var array
      */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+    protected $dontFlash = ['password', 'password_confirmation'];
 
     /**
      * Report or log an exception.
@@ -50,6 +55,84 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+            return response()->json(
+                [
+                    'result' => 0,
+                    'error' => $errors,
+                    'data' => [],
+                ],
+                422
+            );
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $model = class_basename($exception->getModel());
+            return response()->json(
+                [
+                    'result' => 0,
+                    'error' => $model . ' not found',
+                    'data' => [],
+                ],
+                404
+            );
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->json(
+                [
+                    'result' => 0,
+                    'error' => 'Unautheticated',
+                    'data' => [],
+                ],
+                401
+            );
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json(
+                [
+                    'result' => 0,
+                    'error' => $exception->getMessage(),
+                    'data' => [],
+                ],
+                403
+            );
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json(
+                [
+                    'result' => 0,
+                    'error' => 'This URL does not exist',
+                    'data' => [],
+                ],
+                404
+            );
+        }
+
+        if ($exception instanceof HttpException) {
+            return response()->json(
+                [
+                    'result' => 0,
+                    'error' => $exception->getMessage(),
+                    'data' => [],
+                ],
+                $exception->getStatusCode()
+            );
+        }
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json(
+                [
+                    'success' => 0,
+                    'message' =>
+                        'Method is not allowed ,please use supported request method.',
+                ],
+                405
+            );
+        }
+
         return parent::render($request, $exception);
     }
 }
