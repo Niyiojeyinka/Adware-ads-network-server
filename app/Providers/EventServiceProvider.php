b92f97b729b6c2f4ed0708208dad9e62832dc1d0
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-08-14 13:52:50 +0100

    registered sendrecover email listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,8 +15,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        Registered::class => [SendEmailVerificationNotification::class],
+        \App\Events\NewUserRegister::class => [
+            \App\Listeners\SendRecoverPasswordEmail::class,
         ],
     ];
 
