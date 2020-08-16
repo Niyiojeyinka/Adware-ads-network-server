f2fad54effa5c0034cd10d3600ece68ad1990f7b
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-08-16 19:21:44 +0100

    updated passwordreset db resources

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email')->unique();
             $table->string('token');
             $table->timestamps();
         });
